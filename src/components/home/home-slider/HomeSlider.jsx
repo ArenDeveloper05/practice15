fcaf31d6e67206c037aebf3b676e5bfc8721edcf
ArmHos <arh20025@gmail.com>
2023-10-07 20:33:24 +0400

    Added new section for home page HomeSlider,modified favicon.ico changed to new icon,changed the title of page

--- src/components/home/home-slider/HomeSlider.jsx
+++ src/components/home/home-slider/HomeSlider.jsx
@@ -1,11 +1,46 @@
-import Container from "../../common/container/Container";
+import "./HomeSlider.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, A11y, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { CONFIG } from "../../../config";
+import Slide from "./slide/Slide";
 
 const HomeSlider = () => {
+  const homeSliderData = CONFIG.home_slider_data;
   return (
-    <section>
-      <Container>
-        {/* KODY GRUM ENQ AYSTEX, QANI VOR CONTAINER COMPONENTY APAHOVEL E MAX-WIDTH EV MARGIN: AUTO */}
-      </Container>
+    <section className="home-slider">
+      <Swiper
+        modules={[Navigation, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        loop={true}
+        grabCursor
+        autoplay={{
+          delay: 5000,
+        }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={(swiper) => {
+          console.log(swiper.activeIndex);
+        }}
+      >
+        {homeSliderData.map(({ id, title, main_image, desc, icons }) => {
+          return (
+            <SwiperSlide key={id}>
+              <Slide
+                id={id}
+                title={title}
+                main_image={main_image}
+                desc={desc}
+                icons={icons}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
