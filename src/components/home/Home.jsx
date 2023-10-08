063a7af82acb5cd037dda809e4df59751cda0b38
MuzanSamaa <139350216+MuzanSamaa@users.noreply.github.com>
2023-10-08 18:31:36 +0400

    Completed latest news section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.scss";
+import { LatestNews } from "./home-latest-news/LatestNews";
 import HomeSlider from "./home-slider/HomeSlider";
 
 const Home = () => {
   return (
     <div className="home">
       <HomeSlider />
+      <LatestNews/>
       {/* kody grelu eq aystex, baci headeric ev footeric, qani vor nranq gtnvum en Layout-i mej!!!! */}
     </div>
   );
--- src/components/home/home-slider/HomeSlider.jsx
+++ src/components/home/home-slider/HomeSlider.jsx
@@ -4,6 +4,7 @@ const HomeSlider = () => {
   return (
     <section>
       <Container>
+        
         {/* KODY GRUM ENQ AYSTEX, QANI VOR CONTAINER COMPONENTY APAHOVEL E MAX-WIDTH EV MARGIN: AUTO */}
       </Container>
     </section>
