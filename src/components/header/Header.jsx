9e2c37fdd2c9bb3000df8916cb3d6a6a004639b7
Aram Sahakyan <aramsahakyan712@gmail.com>
2023-10-09 21:00:27 +0400

    changed header position on scroll, with it's styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,31 @@ import Menu from "./menu/Menu";
 import Blog from "./blog/Blog";
 import Pages from "./pages/Pages";
 import { CONFIG } from "../../config";
+import { useEffect, useState } from "react";
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    // Function to handle the scroll event
+    const handleScroll = () => {
+      // Check if the scroll position is greater than 0
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    // Add the scroll event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <header>
+    <header className={scrolled ? "fixed" : ""}>
       <Container>
        <div className="row">
         <div className="logo">
