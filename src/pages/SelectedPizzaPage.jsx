7731a23edba3df1fde28fc217d710d23de883f85
ArenDeveloper05 <antisystem005@mail.ru>
2023-10-13 19:09:35 +0400

    single pizza mistakes

--- src/pages/SelectedPizzaPage.jsx
+++ src/pages/SelectedPizzaPage.jsx
@@ -1,20 +1,15 @@
-import { useParams } from 'react-router-dom';
-import SelectedPizza from '../components/menu/selected-pizza/SelectedPizza'
-import Layout from '../layout/Layout'
-import { useSelector } from 'react-redux';
+import { useParams } from "react-router-dom";
+import SelectedPizza from "../components/menu/selected-pizza/SelectedPizza";
+import Layout from "../layout/Layout";
 
 const SelectedPizzaPage = () => {
-  const {id} = useParams();
-  const data = useSelector(state => state.pizzaReview.pizzas);
-  const x = data.filter(item => item.id === id);
-  data.forEach((item) => {
-    if(item.id === id) console.log(item);
-  })
-  console.log(x);
-  // console.log(id);
-  return <Layout>
-    <SelectedPizza id={id}/>
-  </Layout>
-}
+  const { id } = useParams();
 
-export default SelectedPizzaPage
+  return (
+    <Layout>
+      <SelectedPizza id={id} />
+    </Layout>
+  );
+};
+
+export default SelectedPizzaPage;
--- src/redux/slices/pizzaReviewSlice.js
+++ src/redux/slices/pizzaReviewSlice.js
@@ -8,76 +8,82 @@ import margherita from "../../assets/header-images/menu-list-images/margherita.p
 import { useParams } from "react-router-dom";
 
 const initialState = {
-    pizzas: [
-        {
-          id: 1,
-          img: pepperoni,
-          name: "Pepperoni",
-          price: "$12.99",
-          last_price: "$30.50",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-          id: 2,
-          img: vegetarian,
-          name: "Vegetarian",
-          price: "$12.99",
-          last_price: "$30.50",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-          id: 3,
-          img: specialty,
-          name: "Specialty",
-          price: "$12.99",
-          last_price: "$30.50",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-          id: 4,
-          img: hamCheese,
-          name: "Ham & Cheese",
-          price: "$12.99",
-          last_price: "$30.50",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-          id: 5,
-          img: onion,
-          name: "Onion",
-          price: "$12.99",
-          last_price: "$30.50",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-        {
-          id: 6,
-          img: margherita,
-          name: "Margheritapizza",
-          price: "$12.99",
-          last_price: "$30.50",
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry."
-        },
-    ],
-    filteredPizza: []
-}
+  pizzas: [
+    {
+      id: 1,
+      img: pepperoni,
+      name: "Pepperoni",
+      price: "$12.99",
+      last_price: "$30.50",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 2,
+      img: vegetarian,
+      name: "Vegetarian",
+      price: "$12.99",
+      last_price: "$30.50",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 3,
+      img: specialty,
+      name: "Specialty",
+      price: "$12.99",
+      last_price: "$30.50",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 4,
+      img: hamCheese,
+      name: "Ham & Cheese",
+      price: "$12.99",
+      last_price: "$30.50",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 5,
+      img: onion,
+      name: "Onion",
+      price: "$12.99",
+      last_price: "$30.50",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravid: 1, mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 6,
+      img: margherita,
+      name: "Margheritapizza",
+      price: "$12.99",
+      last_price: "$30.50",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lorem nunc. ipsum primis in faucibus orci luctus et ultricesLorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+  ],
+  filteredPizza: [],
+};
 
 const pizzaReviewSlice = createSlice({
-    name: "pizzaReview",
-    initialState,
-    reducers: {
-        // filter: (state, {payload}) => {
-        //     if(payload === "all") {
-        //        state.filteredPizza = state.specialMenu 
-        //     }else{
-        //         const filteredBydesserts = state.specialMenu.filter((item) => item.type === payload)
-        //         state.filteredPizza = filteredBydesserts;
-        //     }
-        // },
-        filter: (state, {payload}) => {
-            state.filteredPizza = state.pizzas.filter((item) => item.id === payload);
-        }
-    }
-})
+  name: "pizzaReview",
+  initialState,
+  reducers: {
+    // filter: (state, {payload}) => {
+    //     if(payload === "all") {
+    //        state.filteredPizza = state.specialMenu
+    //     }else{
+    //         const filteredBydesserts = state.specialMenu.filter((item) => item.type === payload)
+    //         state.filteredPizza = filteredBydesserts;
+    //     }
+    // },
+    filter: (state, { payload }) => {
+      state.filteredPizza = state.pizzas.filter((item) => item.id === payload);
+    },
+  },
+});
 
 export default pizzaReviewSlice.reducer;
-export const {filter} = pizzaReviewSlice.actions;
+export const { filter } = pizzaReviewSlice.actions;
